12f6a6de7a57d40976f944bd318f93340bc44a2c
ryosuke <ryosuke@example.com>
2025-11-06 13:48:07 +0900

    feat: 保存アイコンをフロッピー風に更新

--- apps/web/components/icons/save-icon.tsx
+++ apps/web/components/icons/save-icon.tsx
@@ -12,13 +12,13 @@ function SaveIcon({ className = "w-6 h-6" }: BaseIconProps) {
       strokeLinejoin="round"
       className={className}
     >
-      <path d="M1 3h9a2 2 0 0 1 2 2v16a1 1 0 0 0-1-1H1z" fill="none" />
-      <path d="M23 3h-9a2 2 0 0 0-2 2v16a1 1 0 0 1 1-1h10z" fill="none" />
-      <path d="M12 3v18" />
-      <path d="M4 9h5m-5 3h4" />
-      <path d="M15 9h5m-5 3h4" />
-      <path d="M18 1l5 5-7 7-4 1 1-4z" fill="none" />
-      <path d="M20 3l3 3" />
+      <path
+        d="M5 3h10.586a2 2 0 0 1 1.414.586L20.414 6a2 2 0 0 1 .586 1.414V19a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2z"
+        fill="none"
+      />
+      <path d="M7 3v5h10" />
+      <path d="M17 21v-8H7v8" />
+      <path d="M9 13h6" />
     </svg>
   );
 }
